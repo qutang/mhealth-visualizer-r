f0ba74018a1080f315150a63693ceec368867830
Qu Tang <tqshelly@gmail.com>
2016-09-09 11:48:08 -0400

    Remove useless files

--- inst/counts_script/WalkingRunningCountsAnalysis/determine_segments.r
+++
@@ -1,3 +0,0 @@
-testData = SensorData.importActigraphCsv("../../MATLAB/counts/consistency_test/data/P1-2/DIEGO_DOMINANT_WAIST_40 (2016-03-25)RAW.csv")
-clipped = SensorData.clip(testData, startTime = "2016-03-25 16:00:00", endTime = "2016-03-25 16:10:30")
-SensorData.ggplot(clipped)
